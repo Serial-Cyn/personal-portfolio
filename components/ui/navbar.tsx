7a97fa40b184521391726648c5946b4c13fa6440
Christian Mamplata <201923244+Serial-Cyn@users.noreply.github.com>
2025-09-30 21:02:41 +0800

    feat: add navbar component and integrate into layout
    Included:
    - Logo
    - About
    - Expertise
    - Projects
    - Contact

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,29 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="fixed top-0 w-screen h-auto flex justify-center items-center p-6">
+        <div className="w-1/4">
+            <h1 className="text-lg font-light tracking-wider pointer-events-none select-none">CHRISTIAN</h1>
+            <h1 className="pl-4 text-lg font-light tracking-wider pointer-events-none select-none">MAMPLATA</h1>
+
+        </div>
+        <div className="w-full">
+            <ul className="w-full flex justify-end items-center text-lg font-medium space-x-16">
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#expertise">Expertise</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a className="px-4 py-2 bg-purple-400 hover:bg-purple-500 text-gray-900 rounded-full" href="#contact">Get in Touch!</a>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
