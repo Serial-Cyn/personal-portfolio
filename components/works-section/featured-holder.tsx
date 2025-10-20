e053d38ca56cc14331a117e57464f5c539574141
Christian Mamplata <201923244+Serial-Cyn@users.noreply.github.com>
2025-10-20 15:33:21 +0800

    feat: add stacking animation for featured works

--- components/works-section/featured-holder.tsx
+++ components/works-section/featured-holder.tsx
@@ -1,16 +1,75 @@
+'use client';
 
-type HolderProps = {
-    imgSrc: string;
-    title: string;
-    link: string;
-};
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
-export default function Holder({ imgSrc, title, link }: HolderProps) {
+gsap.registerPlugin(ScrollTrigger);
+
+// List of featured works
+const featuredWorks = [
+    {
+        img: '/featured/monobit-splash.webp',
+        title: "Monobit",
+        link: '',
+    },
+    {
+        img: '/featured/monobit-splash.webp',
+        title: "Java Heroes",
+        link: '',
+    },
+]
+
+export default function Holder() {
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        // Select all sections to be pinned
+        const sections = gsap.utils.toArray<HTMLElement>('.featured-section');
+
+        // Pin each section as the user scrolls
+        sections.forEach((panel) => {
+            ScrollTrigger.create({
+                trigger: panel,
+                start: "top top",
+                pin: true,
+                pinSpacing: false,
+            });
+
+            // Fade out the last panel
+            const lastPanel = sections[sections.length - 1];
+
+            gsap.to(lastPanel, {
+                opacity: 0,
+                yPercent: 0,
+                ease: "power4.out",
+                scrollTrigger: {
+                    trigger: lastPanel,
+                    start: "center center",
+                    end: "center bottom",
+                    scrub: true,
+                },
+            });
+        });
+
+        // Cleans up ScrollTriggers on unmount
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        }
+
+    });
+
 
     return (
-        <div className="relative min-w-full min-h-full h-screen flex flex-col justify-center items-center overflow-hidden">
-            <img className="absolute inset-0 object-cover w-full h-full brightness-50 z-0" src={imgSrc} alt={title} />
-            <h1 className="w-full text-8xl text-center font-bold uppercase z-10">{title}</h1>
+        <div ref={containerRef} className="relative min-w-full min-h-full overflow-hidden">
+            {featuredWorks.map((work, index) => (
+                <div key={index} className="featured-section w-full h-full min-h-screen flex flex-col justify-center items-center">
+                    <img className="absolute inset-0 object-cover w-full h-full brightness-50 rounded-4xl z-0" src={work.img} alt={work.title} />
+                    <h1 className="relative w-full text-6xl lg:text-8xl text-center font-bold uppercase z-10">{work.title}</h1>
+                </div>
+            ))}
         </div>
     );
 }
+
--- components/works-section/featured-works.tsx
+++ components/works-section/featured-works.tsx
@@ -5,7 +5,7 @@ export default function FeaturedWorks() {
     return (
         <section id="featured-works" className="min-w-full h-auto mt-32 ">
             <h1 className="mb-6 px-6 text-6xl sm:text-8xl font-bold">FEATURED WORKS</h1>
-            <Holder imgSrc="/featured/monobit-splash.webp" title="Monobit" link="" />
+            <Holder />
         </section>
     );
 }
